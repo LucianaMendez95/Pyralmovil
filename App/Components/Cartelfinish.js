8381996a3a1859a5ba51d58e3d013fdc3e8d93a8
Luciana Mendez <lucianamendez95@hotmail.com>
2020-10-08 15:56:30 -0300

    estilos

--- App/Components/Cartelfinish.js
+++ App/Components/Cartelfinish.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from "styled-components"
 import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
 import Adress from './Adress';
+import { AntDesign } from '@expo/vector-icons'; 
 
 
 export default function Cartelfinish() {
@@ -9,6 +10,7 @@ export default function Cartelfinish() {
  
     return (
         <View style={styles.ropaDelCarrito}>
+           <AntDesign name="like1" size={60} color="black" style={{marginBottom:50}} />
             <Text style={styles.title}>Thank you for your purchase!</Text>
             <Text style={styles.info}>You will recieve an email with all the information</Text>
             <Text style={styles.info}>Please comunicate if something went wrong or if you need help</Text>
@@ -20,7 +22,7 @@ const styles = StyleSheet.create({
    
     ropaDelCarrito:{
         backgroundColor:'white',
-        height: 500,
+        height: 350,
         width: 300,
         borderRadius:15,
         display:"flex",
